d6b478f02eac28916dc66fac53f7fcfab7169934
a1073355 <100365796+a1073355@users.noreply.github.com>
2022-02-25 00:43:03 +0800

    Add files via upload

---
+++ Untitled-1.php
@@ -0,0 +1,21 @@
+<html>
+
+<head></head>
+
+<body>
+
+<?php
+
+$j=10;
+$i=30;
+echo "<b>hello</b> <h3>php!!</h3>";
+echo $i+$j;
+echo "</br>";
+echo "</br>";
+echo date("Y,M,D");
+?>
+
+</body>
+
+
+</html>
