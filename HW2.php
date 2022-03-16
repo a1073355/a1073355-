33ffafd3269602dd48fd1704e53f87838f30514b
a1073355 <100365796+a1073355@users.noreply.github.com>
2022-03-16 15:38:58 +0800

    Add files via upload

---
+++ HW2.php
@@ -0,0 +1,72 @@
+<html>
+<head>
+    <title>旅遊</title>   
+    <link rel="shortcut icon" href="D:\XAMPP\htdocs\logo.JFIF"  type="image/x-icon"/>
+</head>
+<body>   
+
+    <?php 
+        echo "<body bgcolor='#87ceeb'>";
+    ?>
+    <a name="#top">
+    <center><h1><b><font color="white">~墾丁三日遊~</font></b></h1></center>
+    <h2 style="text-align: center;"><span style="color: #008000; font-size: 20px;"><strong>墾丁三日遊第一天路線：</strong></span></h2>
+    <div style="background-color:pink;"><p style="text-align: center;"><strong>愛琴海岸-->鹿境-->邱家生魚片-->台電南部展示館-->墾丁公園-->姊妹沙灘(住宿)</strong></p></div><br/>
+    <a href="https://www.facebook.com/%E6%84%9B%E7%90%B4%E6%B5%B7%E5%B2%B8%E6%B5%B7%E6%99%AF%E4%BC%91%E9%96%92%E6%B8%A1%E5%81%87%E5%9C%92%E5%8D%80-604331433292447/"><img src="D:\XAMPP\htdocs\1.jpg" style="position:relative;left:120px;"width="350" height="250"></a>
+    <a href="https://www.facebook.com/hoteldedeer/"><img src="D:\XAMPP\htdocs\2.jpg" style="position:relative;left:150px;"width="350" height="250"></a>
+    <a href="https://www.facebook.com/%E5%BE%8C%E5%A3%81%E6%B9%96%E9%82%B1%E5%AE%B6%E7%94%9F%E9%AD%9A%E7%89%87%E7%B8%BD%E5%BA%97-384219135241/"><img src="D:\XAMPP\htdocs\3.jpg" style="position:relative;left:180px;"width="350" height="250"></a><br/>
+    <a href="https://wapp4.taipower.com.tw/nsis/south/index.html"><img src="D:\XAMPP\htdocs\4.jpg"style="position:relative;left:120px;" width="350" height="250"></a>
+    <a href="https://www.ktnp.gov.tw/"><img src="D:\XAMPP\htdocs\5.jpg" style="position:relative;left:150px;"width="350" height="250"></a>
+    <a href="https://www.4sistersvilla.com/tw/index.html"><img src="D:\XAMPP\htdocs\6.jpg" style="position:relative;left:180px;"width="350" height="250"></a>
+    <p style="text-align: center;"><iframe loading="lazy" style="border: 0;" tabindex="0" src="https://www.google.com/maps/embed?pb=!1m46!1m12!1m3!1d236607.79896692603!2d120.60358533128192!3d22.087936928051246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m31!3e0!4m5!1s0x3471c500763dab03%3A0xd52ebced50458122!2z5oSb55C05rW35bK45rW35pmv5LyR6ZaS5rih5YGH5ZyS5Y2A!3m2!1d22.239704!2d120.668101!4m5!1s0x3471b0fc5492b6f9%3A0xf017e258e04f8bf6!2z6bm_5aKD5qKF6Iqx6bm_55Sf5oWL5ZyS5Y2A!3m2!1d22.012439399999998!2d120.7416877!4m5!1s0x3471b0387e36334b%3A0xc4ad547336e2400d!2z5b6M5aOB5rmW6YKx5a6255Sf6a2a54mHIFFpdSdzIFNhc2hpbWktL-Wxj-adsee-jumjny_lsY_mnbHlv4XlkIMv5bGP5p2x5bCP5ZCDL-Wxj-adseeUn-mtmueJhy_lsY_mnbHmtbfprq7ppJDlu7M!3m2!1d21.9458898!2d120.74248519999999!4m5!1s0x3471b047af6f97a1%3A0x3770d0ad8fc1be0d!2z5Y-w6Zu75Y2X6YOo5bGV56S66aSo!3m2!1d21.948987199999998!2d120.7442557!4m5!1s0x3471b23b97ce7acb%3A0x2396c1d9953b743c!2z5bGP5p2x57ij5oGG5pil6Y6u6Ii55biG6Lev5aK-5LiBLeWniuWmueaymeeBmOWIpeWihSAoNCBTaXN0ZXJzIFZpbGxhKQ!3m2!1d21.936873!2d120.81917999999999!5e0!3m2!1szh-TW!2stw!4v1607929138857!5m2!1szh-TW!2stw" width="600" height="450" frameborder="0" allowfullscreen="allowfullscreen" aria-hidden="false"></iframe></p>
+
+
+    <h2 style="text-align: center;"><span style="color: #008000; font-size: 20px;"><strong>墾丁三日遊第二天路線：</strong></span></h2>
+    <div style="background-color:pink;"><p style="text-align: center;"><strong>鵝鑾鼻燈塔-->水蛙窟大草原-->龍磐公園-->日日海旅-->夢幻島-->悠活(住宿)</strong></p></div><br/>
+    <a href="https://www.ktnp.gov.tw/News_Content2.aspx?n=5C28A0E32DF925F9&sms=C88B5251F308CE96&s=CC1B26C5C0691F1E"><img src="D:\XAMPP\htdocs\7.jpg"  style="position:relative;left:120px;"width="350" height="250"></a>
+    <a href="https://www.settour.com.tw/travel_guide/%E5%8F%B0%E7%81%A3/%E6%B0%B4%E8%9B%99%E7%AA%9F%E5%A4%A7%E8%8D%89%E5%8E%9F/attractions_POI0000368913.html"><img src="D:\XAMPP\htdocs\8.jpg" style="position:relative;left:150px;" width="350" height="250"></a>
+    <a href="https://www.ktnp.gov.tw/News_Content2.aspx?n=28AB1D16ECF7E63C&sms=C88B5251F308CE96&s=C1E50972440D43B2"><img src="D:\XAMPP\htdocs\9.jpg"style="position:relative;left:180px;" width="350" height="250"></a><br/>
+    <a href="https://www.facebook.com/oceandayhotel/"><img src="D:\XAMPP\htdocs\10.jpg" style="position:relative;left:120px;"width="350" height="250"></a>
+    <a href="https://uukt.com.tw/kenting/47673"><img src="D:\XAMPP\htdocs\11.jpg"style="position:relative;left:150px;" width="350" height="250"></a>
+    <a href="https://www.yoho.com.tw/zh-tw"><img src="D:\XAMPP\htdocs\12.jpg"style="position:relative;left:180px;" width="350" height="250"></a>    
+    <p style="text-align: center;"><iframe loading="lazy" style="border: 0;" tabindex="0" src="https://www.google.com/maps/embed?pb=!1m52!1m12!1m3!1d59193.02477162782!2d120.73768447661536!3d21.989688728104273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m37!3e0!4m5!1s0x3471b23b97ce7acb%3A0x2396c1d9953b743c!2z5bGP5p2x57ij5oGG5pil6Y6u6Ii55biG6Lev5aK-5LiBLeWniuWmueaymeeBmOWIpeWihSAoNCBTaXN0ZXJzIFZpbGxhKQ!3m2!1d21.936873!2d120.81917999999999!4m5!1s0x3471b1150ebf5f65%3A0x5757d23a8bc2a410!2z6bWd6ZG-6by754eI5aGU!3m2!1d21.9022339!2d120.8526297!4m5!1s0x3471b246aaaaaaab%3A0xfeede969654cac2d!2z5rC06JuZ56qf5aSn6I2J5Y6f!3m2!1d21.9343516!2d120.83726899999999!4m5!1s0x3471b25eb23e10a1%3A0x1c92a0d803d845de!2z5bGP5p2x57ij5oGG5pil6Y6u5Y-wMjbnt5rpvo3no5DlhazlnJI!3m2!1d21.9272344!2d120.84796279999999!4m5!1s0x3471b3ccb7df7827%3A0xb664efcb60d9f25e!2z5bGP5p2x57ij5ru_5bee6YSJ6IiI5rW36LevT2NlYW4gRGF5IGJ5IERheSDml6Xml6Xml4Xmtbc!3m2!1d21.9668247!2d120.8419843!4m5!1s0x3471b0b307aa29af%3A0x8bf7b2640077039c!2z5aK-5LiB5aSi5bm75bO2!3m2!1d22.0105985!2d120.72711079999999!5e0!3m2!1szh-TW!2stw!4v1607929267988!5m2!1szh-TW!2stw" width="600" height="450" frameborder="0" allowfullscreen="allowfullscreen" aria-hidden="false"></iframe></p>
+
+
+    <h2 style="text-align: center;"><span style="color: #008000; font-size: 20px;"><strong>墾丁三日遊第三天路線：</strong></span></h2>
+    <div style="background-color:pink;"><p style="text-align: center;"><strong>退房-->阿信巧克力農場-->海洋生物博物館-->回家</strong></p></div><br/>
+    <a href="https://www.taiwanchoco.com.tw/zh-tw"><img src="D:\XAMPP\htdocs\13.jpg"style="position:relative;left:400px;" width="350" height="250"></a>
+    <a href="https://www.nmmba.gov.tw/"><img src="D:\XAMPP\htdocs\14.jpg" style="position:relative;left:420px;"width="350" height="250"></a>
+    <p style="text-align: center;"><iframe loading="lazy" style="border: 0;" tabindex="0" src="https://www.google.com/maps/embed?pb=!1m34!1m12!1m3!1d59184.78320896943!2d120.6749016554828!3d22.009435308440406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m19!3e0!4m5!1s0x3471b0b307aa29af%3A0x8bf7b2640077039c!2z5aK-5LiB5aSi5bm75bO2!3m2!1d22.0105985!2d120.72711079999999!4m5!1s0x3471ba0cbdca6abb%3A0x97f6f4695defe5da!2z5bGP5p2x57ij5oGG5pil6Y6u6JCs6YeM6Lev6Zi_5L-h5ben5YWL5Yqb6L6y5aC0!3m2!1d21.9975132!2d120.7062198!4m5!1s0x3471b9c4a1c1e32f%3A0x3fc2d412a3a7a1c4!2z5bGP5p2x57ij6LuK5Z-O6YSJ5b6M54Gj6Lev5rW35rSL55Sf54mp5Y2a54mp6aSo!3m2!1d22.0460173!2d120.69890579999999!5e0!3m2!1szh-TW!2stw!4v1607929561833!5m2!1szh-TW!2stw" width="600" height="450" frameborder="0" allowfullscreen="allowfullscreen" aria-hidden="false"></iframe></p>
+    
+
+    <form action="index2.php" method="post">
+        Please input your name:<input type="text" name="uname" placeholder="請輸入名字" required>
+        <br/>Please input your password:<input type="password" name="pwd" placeholder="請輸入密碼" required>
+        <br/>Please input your email:<input type="email" name="email"placeholder="請輸入電子信箱" required>
+        <br/>Please input date:<input type="date" name="date" placeholder="請輸入日期">
+        <br/>Please input time:<input type="time" name="time" placeholder="請輸入時間">
+        <br/>Please choose your sex:男性<input type="radio" name="sex" checked>女性<input type="radio" name="sex"> 不分<input type="radio" name="sex">
+        <br/>Please input range:<input type="range" name="range">
+        請輸入預算(單位為百)
+        <br/>Please choose your food preference:葷食<input type="checkbox" name="order" checked>素食<input type="checkbox" name="order">都可<input type="checkbox" name="order">
+        <br/><b>選擇團T顏色</b>
+        <select name="color" width="40" multiple>
+            <option>白色</option>
+            <option>黑色</option>
+            <option>綠色</option>
+            <option>藍色</option>
+            <option>灰色</option>
+        </select><br/>
+    </form>
+    <br/><b>T-shirt對照表</b>
+    <table border="1">
+    <tr><th></th><th>S</th><th>M</th><th>L</th><th>XL</th></tr>
+    <tr><td>長</td><td>50</td><td>60</td><td>70</td><td>80</td></tr>
+    <tr><td>寬</td><td>50</td><td>60</td><td>70</td><td>80</td></tr>
+    <tr><td>高</td><td>60</td><td>70</td><td>80</td><td>90</td></tr>
+    </table>    
+
+    <br/><center><input type="submit"><input type="reset" style="position:relative;left:20px;"></center>
+</body>
+
+</html>
