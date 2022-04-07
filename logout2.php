1ac843a379128368add9296a9c7f3ad4bb827463
a1073355 <100365796+a1073355@users.noreply.github.com>
2022-04-07 10:27:16 +0800

    Add files via upload

---
+++ logout2.php
@@ -0,0 +1,37 @@
+<?php
+$acc=$_POST["account"];
+$pass=$_POST["pwd"];
+$email=$_POST["email"];
+$phone=$_POST["tel"];
+
+
+echo "<center><h1>註冊資訊</h1></center>";
+echo "你的帳號:".$acc."</br>";
+echo "你的密碼".$pass."<br/>";
+echo "你的電子信箱:".$email."<br/>";
+echo "你的電話號碼:".$phone."<br/>";
+//echo "食物:".$foods."<br/>";
+
+
+?>
+
+
+<html>
+<head>
+    
+</head>
+<body>   
+
+   
+
+<form action="login2.php" method="post" enctype="multipart/form-data">
+
+        <br/><center><input type="submit" value="進入登入介面"></center>
+    </form>
+
+
+   
+    
+</body>
+
+</html>
