061ab5eb070f08d6cbb1657370767f176d7fc82c
a1073355 <100365796+a1073355@users.noreply.github.com>
2022-06-19 16:55:21 +0800

    Add files via upload

---
+++ enroll.php
@@ -0,0 +1,10 @@
+<h1>使用者註冊</h1>
+<form action="enrollconfirm.php" method="post">
+
+    輸入名字: <input type="text" name="uName" ></br>
+    輸入密碼: <input type="password" name="uPwd" ></br>
+    輸入角色 : 使用者 <input type="radio" name="role" value="user"></br>
+    管理者<input type="radio" name="role" value="admin"></br>
+
+    <input type="submit" value="註冊去"></br>
+</form>
